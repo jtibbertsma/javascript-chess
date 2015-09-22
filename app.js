2752bd3784b4f5f10722936453635c388cf3cb4b
Joseph Tibbertsma <josephtibbertsma@gmail.com>
2015-09-22 12:19:27 -0700

    Minor reorganization

--- app.js
+++ app.js
@@ -1,28 +1,33 @@
 angular.module('JSChess', [])
 
-  .factory('chessView', [function () {
-    var view = [];
+  .factory('chessView', [
+    function chessViewFactory() {
+      var view = [];
 
-    function isWhite(idx) {
-      if (Math.floor(idx / 8) % 2 === 0) {
-        idx += 1;
+      function isWhite(idx) {
+        if (Math.floor(idx / 8) % 2 === 0) {
+          idx += 1;
+        }
+
+        return idx % 2 === 1;
       }
 
-      return idx % 2 === 1;
+      for (var i = 0; i < 64; ++i) {
+        view.push({
+          content: "",
+          selected: false,
+          destination: false,
+          whiteSquare: isWhite(i),
+          whitePiece: false
+        });
+      }
+
+      return view;
     }
+  ])
 
-    for (var i = 0; i < 64; ++i) {
-      view.push({
-        content: "",
-        selected: false,
-        whiteSquare: isWhite(i),
-        whitePiece: false
-      });
+  .controller('BoardCtrl', ['$scope', 'chessView',
+    function BoardCtrl($scope, chessView) {
+      $scope.view = chessView;
     }
-
-    return view;
-  }])
-
-  .controller('BoardCtrl', ['$scope', 'chessView', function ($scope, chessView) {
-    $scope.view = chessView;
-  }]);
+  ]);
