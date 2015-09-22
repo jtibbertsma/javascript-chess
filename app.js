c9555676d454162c339b640c4237cb1c17e0174f
Joseph Tibbertsma <josephtibbertsma@gmail.com>
2015-09-21 17:55:02 -0700

    Create a chess board view

--- app.js
+++ app.js
@@ -1,1 +1,28 @@
-var app = angular.module('JSChess', []);
+angular.module('JSChess', [])
+
+  .factory('chessView', [function () {
+    var view = [];
+
+    function isWhite(idx) {
+      if (Math.floor(idx / 8) % 2 === 0) {
+        idx += 1;
+      }
+
+      return idx % 2 === 1;
+    }
+
+    for (var i = 0; i < 64; ++i) {
+      view.push({
+        content: "",
+        selected: false,
+        whiteSquare: isWhite(i),
+        whitePiece: false
+      });
+    }
+
+    return view;
+  }])
+
+  .controller('BoardCtrl', ['$scope', 'chessView', function ($scope, chessView) {
+    $scope.view = chessView;
+  }]);
