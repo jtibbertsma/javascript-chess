f3c95616d98af34ccc195fdb057673ed8cec1c3d
Joseph Tibbertsma <josephtibbertsma@gmail.com>
2015-09-27 20:08:49 -0700

    Wrote specs for king and queen

--- spec/chess/unit/pieces/king.js
+++ spec/chess/unit/pieces/king.js
@@ -1,7 +1,14 @@
 describe("Chess.Pieces.King", function () {
 
   beforeEach(function () {
-    this.king = new Chess.Pieces.King({ color: "white", pos: [0,0] });
+    this.king = new Chess.Pieces.King({ color: "black", pos: [1,1] });
+
+    new Chess.Board({ pieces: [
+      this.king,
+      new Chess.Pieces.Pawn({ color: "black", pos: [2,0] }),
+      new Chess.Pieces.Pawn({ color: "white", pos: [2,1] }),
+      new Chess.Pieces.Pawn({ color: "white", pos: [2,2] })
+    ]});
   });
 
   it("inherits from SteppingPiece", function () {
@@ -10,5 +17,13 @@ describe("Chess.Pieces.King", function () {
 
   it("is a king", function () {
     expect(this.king.isKing()).toEqual(true);
+  });
+
+  describe(".validMoves", function () {
+
+    it("moves like a King", function () {
+      expect(Chess.Util.coordSort(this.king.validMoves()))
+        .toEqual(Chess.Util.coordSort([[2,1], [2,2], [1,2], [0,2], [0,1], [0,0], [1,0]]));
+    });
   });
 });
--- spec/chess/unit/pieces/queen.js
+++ spec/chess/unit/pieces/queen.js
@@ -1,10 +1,27 @@
 describe("Chess.Pieces.Queen", function () {
 
   beforeEach(function () {
-    this.queen = new Chess.Pieces.Queen({ color: "white", pos: [0,0] });
+    this.queen = new Chess.Pieces.Queen({ color: "white", pos: [3,1] });
+
+    new Chess.Board({ pieces: [
+      this.queen,
+      new Chess.Pieces.Pawn({ color: "black", pos: [3,4] }),
+      new Chess.Pieces.Pawn({ color: "white", pos: [5,1] })
+    ]});
   });
 
   it("inherits from SlidingPiece", function () {
     expect(this.queen.parentClass).toEqual(Chess.Pieces.SlidingPiece);
+  });
+
+  describe(".validMoves", function () {
+
+    it("moves like a Queen", function () {
+      expect(Chess.Util.coordSort(this.queen.validMoves()))
+        .toEqual(Chess.Util.coordSort([
+          [2,1], [1,1], [0,1], [4,1], [3,0], [4,0], [2,0], [2,2],
+          [1,3], [0,4], [4,2], [5,3], [6,4], [7,5]
+        ]));
+    });
   });
 });
