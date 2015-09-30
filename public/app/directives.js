79e9b007e4676f46be6b4926744d07758d377f8e
Joseph Tibbertsma <josephtibbertsma@gmail.com>
2015-09-30 07:00:14 -0700

    Add logic to set which squares can be selected

--- public/app/directives.js
+++ public/app/directives.js
@@ -1,19 +1,35 @@
 angular.module('ChessDirectives', [])
-  .directive('chessBoardView',
-    function chessBoardViewDirective() {
+  .directive('chessBoardView', ['gameData',
+    function chessBoardViewDirective(gameData) {
+      function idxToArr(i, j) {
+        return i * 8 + j;
+      }
+
       return {
         templateUrl: '/templates/square.html',
-        link: function (scope, element) {
-          element.addClass("chess-board");
+        link: function (scope, element, attrs) {
+          attrs.$addClass("chess-board");
           scope.squares = [];
+          scope.player = "white";
 
           for (var i = 0; i < 64; ++i) {
-            scope.squares.push({ piece: null });
+            scope.squares.push({
+              piece: null,
+              selectable: false
+            });
           }
+
+          scope.setSelectable = function () {
+            gameData.game.movablePositions(scope.player).forEach(function (pos) {
+              scope.squares[idxToArr(pos[0], pos[1])].selectable = true;
+            });
+          }
+
+          scope.setSelectable();
         }
       };
     }
-  )
+  ])
 
   .directive('boardSquare', ['gameData',
     function boardSquareDirective(gameData) {
@@ -25,14 +41,13 @@ angular.module('ChessDirectives', [])
       }
 
       return {
-        link: function (scope, element) {
-          element.addClass("square");
+        link: function (scope, element, attrs) {
+          attrs.$addClass("square");
+          attrs.$addClass(isWhite(scope.$index) ? "white" : "black")
 
-          if (isWhite(scope.$index)) {
-            element.addClass("white");
-          } else {
-            element.addClass("black");
-          }
+          // if (scope.square.selectable) {
+          //   attrs.$addClass("selectable");
+          // }
         }
       }
     }
