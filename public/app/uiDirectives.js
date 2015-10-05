5cbe4a9a0f113dfe30bed182120e72923a4df843
Joseph Tibbertsma <josephtibbertsma@gmail.com>
2015-10-04 22:47:39 -0700

    Set up message textarea

--- public/app/uiDirectives.js
+++ public/app/uiDirectives.js
@@ -1,1 +1,12 @@
-angular.module('ChessUIDirectives', []);
+angular.module('ChessUIDirectives', [])
+  .directive('chatBoxView', [
+    function chatBoxViewDirective() {
+      return {
+        templateUrl: '/templates/chatBoxContent.html',
+        scope: true,
+        link: function (scope, element, attrs) {
+          scope.messages = [];
+        }
+      };
+    }
+  ]);
