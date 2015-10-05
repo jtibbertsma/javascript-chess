d7205ef609a9f46525a984230e0abd38f4b7ebab
Joseph Tibbertsma <josephtibbertsma@gmail.com>
2015-10-04 23:25:04 -0700

    Messages can be posted

--- public/app/uiDirectives.js
+++ public/app/uiDirectives.js
@@ -9,4 +9,25 @@ angular.module('ChessUIDirectives', [])
         }
       };
     }
-  ]);
+  ])
+
+  .directive('getMessage', [
+    function getMessageDirective() {
+      return {
+        scope: true,
+        link: function (scope, element, attrs) {
+          scope.message = { content: '' };
+          element.on('keydown', keydown);
+
+          function keydown(event) {
+            if (event.keyCode === 13) {
+              event.preventDefault();
+              scope.messages.push(scope.message);
+              scope.message = { content: '' };
+              scope.$apply();
+            }
+          }
+        }
+      }
+    }
+  ])
